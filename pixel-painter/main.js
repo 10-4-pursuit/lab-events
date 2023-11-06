55d39f268fbaf3f6af1d7dc9c36e6d7364ee59a1
EdgarCollado <edgarcollado@pursuit.org>
2023-11-06 16:32:13 -0500

    started-lab-created-functions

---
+++ pixel-painter/main.js
@@ -0,0 +1,15 @@
+function colorSelector() {
+
+}
+
+function createBoard() {
+
+}
+
+function resetBoard() {
+
+}
+
+function changeBoardToCurrentColor() {
+
+}
---
+++ tic-tac-toe/main.js
@@ -0,0 +1,14 @@
+function makeBoard() {
+    const board = document.createElement("div");
+    board.classList.add("board");
+// The code below will create a row.
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let s = 0; s < 3; s++) {
+            const squareElement = document.createElement("div");
+            squareElement
+        }
+    }
+}
