27a3d7c841a80506d7183f93dd1f195b9385d0c2
EdgarCollado <edgarcollado@pursuit.org>
2023-11-08 15:09:53 -0500

    completed-most-functions

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,14 +1,58 @@
-function makeBoard() {
-    const board = document.createElement("div");
-    board.classList.add("board");
-// The code below will create a row.
-    for (let i = 0; i < 3; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
+const gameBoard = document.querySelector(".tic-tac-toe");
 
-        for (let s = 0; s < 3; s++) {
-            const squareElement = document.createElement("div");
-            squareElement
+let isPlayerXTurn = true;
+
+function makeMove(event) {
+    const checkBox = event.target;
+    if (checkBox.classList.contains("empty")) {
+        checkBox.classList.remove("empty");
+        checkBox.textContent = "X";
+        if (isPlayerXTurn) {
+            checkBox.textContent = "X";
+        } else {
+            checkBox.textContent = "O"
         }
+        isPlayerXTurn = !isPlayerXTurn
     }
+}
+
+function makeBoard() {
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+      
+        for (let j = 0; j < 3; j++) {
+          const squareElement = document.createElement('div');
+          squareElement.classList.add('square');
+          squareElement.classList.add('empty');
+      
+          squareElement.addEventListener('click', makeMove);
+      
+          row.appendChild(squareElement);
+        }
+      
+        gameBoard.appendChild(row);
+      }
+}
+
+makeBoard();
+
+function resetBoard() {
+    const rowSelect = document.querySelectorAll(".row")
+    for (let rows of rowSelect) {
+        rows.remove()
+    }
+    makeBoard();
+}
+
+const resetButton = document.querySelector("button"); 
+
+resetButton.addEventListener("click", resetBoard);
+
+function gameOver() {
+
+}
+
+function announceWinner() {
+    alert("Game over! The board is full.");
 }
